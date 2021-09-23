ef6edae4b058ea3f80cfc18625c96009f55be003
Marian Babik <marian.babik@cern.ch>
2021-09-23 10:07:17 +0200

    increased netstat polling frequency

--- scitags/settings.py
+++ scitags/settings.py
@@ -7,4 +7,4 @@ UDP_FIREFLY_PORT = 10514
 IP4_DISCOVERY = ('10.255.255.255', 1)
 IP6_DISCOVERY = ('fc00::', 1)
 STUN_SERVERS = [('stun.l.google.com', 19305), ('stun.services.mozilla.org', 3478)]
-NETSTAT_TIMEOUT = 10
+NETSTAT_TIMEOUT = 5
