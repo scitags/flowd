4f36694985ad3caeee70684f3dc33c901f4c1e61
Marian Babik <marian-babik@users.noreply.github.com>
2024-09-19 13:36:54 +0200

    new release

--- scitags/__init__.py
+++ scitags/__init__.py
@@ -8,8 +8,8 @@ except ImportError:
 
 AUTHOR = "Marian Babik <Marian.Babik@cern.ch>, Tristan Sullivan <tssulliv@uvic.ca>"
 AUTHOR_EMAIL = "<net-wg-dev@cern.ch>"
-VERSION = "1.1.4"
-DATE = "22 July 2024"
+VERSION = "1.1.5"
+DATE = "19 Sept 2024"
 __author__ = AUTHOR
 __version__ = VERSION
 __date__ = DATE
